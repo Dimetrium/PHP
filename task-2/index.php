eab9dd59c77294f8866e08ce992d389b29ce6d25
Dimetrium <diskakun@gmail.com>
2015-02-04 05:38:55 +0200

    commit a

--- task-2/index.php
+++ task-2/index.php
@@ -1,12 +1,29 @@
 <?php
+//phpinfo();
 require_once 'config.php';
 require_once LIB;
-
+//init and print
 $file = new Editor();
-$file->getLine('3');
-//$file->setChar('5', 'K');
-//$file->getChar('5');
-//$file->getLines('10');
+
+//Select line
+$selectedLine = 3;
+
+//Select char
+$selectCharLine = 3;
+$selectCharPosition = 10;
+
+//replace line
+$getRepLine = 1;
+$replaceLine = 2;
+
+//replace char
+$getCharLine = 0;
+$getCharNum = 5;
+$replacedChar = "K";
+
 
 require_once TMPLT;
+
+$file->exportFile();
+
 ?>
--- task-2/lib/Editor.php
+++ task-2/lib/Editor.php
@@ -1,12 +1,60 @@
 <?php
 class Editor
 {
- private $f_import = F_IMPORT; 
- private $f_export = F_EXPORT;
- public function __construct($f_import)
- {
-  $this->f_iport = 
- } 
+  private $f_import; 
+  private $f_export;
+	
+  public function __construct()
+  {
+    $this->f_import = file(F_IMPORT);
+    $this->f_export = file(F_EXPORT);
+  } 
+	
+//	public function __destruct()
+//	{
+//file_put_contents(F_EXPORT, $this->f_import);
+//	}
+	
+  public function printFile($lineNum = '')
+	{
+		return $this->f_import;
+	}
+	
+  public function getLine($selectLine)
+  {
+    return $this->f_import[$selectLine];
+  }
+	
+	public function getChar($selectCharLine, $selectCharPosition)
+	{
+		return $this->f_import[$selectCharLine][$selectCharPosition];
+	}
+
+	public function replaceLine($getRepLine, $replaceLine)
+	{
+		$this->f_import[$getRepLine] = $this->f_import[$replaceLine];
+		return $this->f_import;
+	}	
+	
+	public function replaceChar($getCharLine, $getCharNum, $replacedChar)
+	{
+		$this->f_import[$getCharLine][$getCharNum] = $replacedChar;
+		return $this->f_import;
+	}
+	
+	public function exportFile()
+	{
+		file_put_contents(F_EXPORT, $this->f_import);
+	}
+
+	public function printExportFile()
+	{
+		return $this->f_export;
+	}
+	
 }
+
+
+
 
 ?>

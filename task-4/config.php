c8f18ae8461bfbae099a50757e7aa94d515761b0
Student 5 <user5@tc.geeksforless.net>
2015-02-09 08:38:14 +0200

    task-444

--- task-4/config.php
+++ task-4/config.php
@@ -4,6 +4,7 @@ define('USER', 'user5');
 define('PASSWORD', 'tuser5');
 define('DATA_BASE', 'user5');
 define('TABLE', 'DataWork');
+define('TEMPLATE', 'template/index.inc.php');
 
 define('COOKIES_DIR', '/var/www/html/PHP/task-4/cookies/');
 ?>
--- task-4/index.php
+++ task-4/index.php
@@ -5,24 +5,18 @@ function __autoload( $class )
   require_once( 'lib/'.$class.'.php' );
 }
 
-//$data = new Cookies();
-//$data->add( 'cookaaa','cookkaa value' );
-//$cook = $data->read( 'cookaaa' );  
-//$data->remove( 'cookaaa' );
-//echo $cook;
+$data = new Cookies();
+$data->add( 'cookaaa','cookkaa value' );
+$cook = $data->read( 'cookaaa' );  
+
 $query = new MySql();
-//$query->add( '4', 'John 4' );
-//$query->add( '5', 'ivan 5' );
-echo $query->read( '5' );
+$query->add( '5', 'ivan 5' );
+
+$session = new Sessions();
+$session->add( '4', '4essTest');
+
+include TEMPLATE;
+
+$data->remove( 'cookaaa' );
 $query->remove( '5' );
-//$session = new Sessions();
-//$session->add( '5', '5essTest');
-//$session->add( '4', '4essTest');
-//$session->add( '6', '6essTest');
-//echo $session->read('4');
-//echo $session->read('5');
-//echo $session->read('6');
-//$session->remove('4');
-//echo $session->read('4');
-//echo $session->read('5');
-//echo $session->read('6');
+$session->remove('4');

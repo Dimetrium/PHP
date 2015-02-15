52a1fd53bdac11a4f92f2bcdfa1397e3af8e4732
Dimetrium <diskakun@gmail.com>
2015-02-15 23:53:34 +0200

    Update table.php

--- task-1/templates/table.php
+++ task-1/templates/table.php
@@ -1,8 +1,40 @@
-<form enctype="multipart/form-data" action="" method="post">
-	<input type="hidden" name="MAX_FILE_SIZE" value="30000"/>
-	<input name="userfile" type="file"/>
-	<input type="submit"/>
-</form>
-<?php
-echo $echo;
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>File Uploader</title>
+</head>
+<body style="text-align: center; weight:960px; margin:0 auto;">
+   <form action="index.php" method="post" enctype="multipart/form-data">
+       <input type="file" name="user_file" />
+       <input type="submit" />
+   </form>
+   <h3><?php echo $var;?></h3>
+   <hr>
+   <table <?=$display?> align="center" border="1" cellpadding="6" frame="1" width="800">
+       <tr>
+           <th>N</th>
+           <th>Name of file</th>
+           <th>Size</th>
+           <th>Delete</th>
+       </tr>
+        <?php
+while($name= readdir($dir)):
+    if(is_dir($name))
+        continue;
+        else
+       ?>
+        <tr><td><?=$count?><br></td>
+        <td><?=$name?><br></td>
+        <td><?=sizefile(filesize("files/$name"))?><br></td>
+        <td><a href="index.php?name=<?=$name?>">Delete</a><br></td></tr>
+    <?php 
+$count++;
+endwhile;
+if($count==1)
+    echo "Is no file";
+closedir($dir);
+    ?>
+   </table>
+   
+</body>
+</html>

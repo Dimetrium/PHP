06d15337564ac9a9e1288e579c90abec158057da
Dimetrium <diskakun@gmail.com>
2015-02-15 23:53:05 +0200

    Update index.php

--- task-1/index.php
+++ task-1/index.php
@@ -1,7 +1,22 @@
 <?php
-require_once 'config.php';
-require_once LIB;
-
-
-require_once TEMPLATE;
+include 'config.php';
+include 'functions.php';
+$display = display();
+$dir = opendir(DIR_DEST);
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $var = upload();
+}
+if(isset($_GET['name'])){
+    $file = $_GET['name'];
+    $file_del = DIR_DEST . $file;
+    if($var = del($file_del)){
+        $var;
+        }else{
+            $var = "Access denided";
+        }
+}
+    //if(isset($_SERVER['HTTP_REFERER'])){
+    //header("location: $_SERVER[HTTP_REFERER]");
+          
+include VIEW;
 ?>

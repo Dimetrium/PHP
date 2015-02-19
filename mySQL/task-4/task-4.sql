dc77e51492fa1e5ad9e14d8872847b2cea0c3ab5
Dimetrium <diskakun@gmail.com>
2015-02-19 14:00:40 +0200

    Create task-4.sql

---
+++ mySQL/task-4/task-4.sql
@@ -0,0 +1,10 @@
+1. select model, speed, hd FROM pc where price < '500';
+2. select distinct maker from product where type = 'Printer';
+3. select model, ram, screen from laptop where price > '1000';
+4. select code, model, color, type, price from printer where color = 'y';
+5. select model, speed, hd from pc where (cd = '12x' or cd = '24x') and price < '600'
+
+30. 
+31. select class, country from classes where bore >= '16';
+32.
+33. Select ship from outcomes where battle = 'North Atlantic' and result = 'sunk'

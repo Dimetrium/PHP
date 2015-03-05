fdd7f82ff4009eb023dc0088b4169c4da00ce0a6
Dimetrium <diskakun@gmail.com>
2015-03-05 17:03:11 +0200

    Update task1.sql

--- mySQL/task-1/task1.sql
+++ mySQL/task-1/task1.sql
@@ -1,18 +1,62 @@
-Тема:
-          MySQL: 
-создание/удаление
-          таблиц; 
-основные
-          типы данных, типы индексов; 
-создание/удаление
-          индексов;
-добавление/удаление
-          колонок в таблицы;
-создание/изменение/удаление
-          юзеров;
-добавление/удаление
-          прав юзеров на таблицы и SQL запросов к ним.
-   
-        
-Практика: Сделать руками БД (дамп) магазина на основе
-        книжного каталога.
+CREATE TABLE `xyz_books` (
+  `id` INT(10) NOT NULL AUTO_INCREMENT,
+  `price` FLOAT,
+  `book_name` TEXT,
+  `img` VARCHAR(255),
+  `content` TEXT,
+  `visible` ENUM('0','1'),
+  PRIMARY KEY  (`id`)
+);
+
+CREATE TABLE `xyz_authors` (
+  `id` INT(10) NOT NULL AUTO_INCREMENT,
+  `name` TEXT,
+  PRIMARY KEY  (`id`)
+);
+
+CREATE TABLE `xyz_genre` (
+  `genre_id` INT(10),
+  `genre_name` TEXT
+);
+
+CREATE TABLE `xyz_book_a` (
+  `author_id` INT(10),
+  `book_id` INT(10)
+);
+
+CREATE TABLE `xyz_book_g` (
+  `genre_id` INT(10),
+  `book_id` INT(10)
+);
+
+CREATE TABLE `xyz_users` (
+  `discount` INT(10),
+  `user_id` INT(10) NOT NULL AUTO_INCREMENT,
+  `password` INT(10),
+  `name` INT(10),
+  PRIMARY KEY  (`user_id`)
+);
+
+CREATE TABLE `xyz_cart` (
+  `book_id` INT(10),
+  `order_id` INT(100) NOT NULL AUTO_INCREMENT,
+  `quantity` INT(10),
+  `user_id` INT(10),
+  PRIMARY KEY  (`order_id`)
+);
+
+CREATE TABLE `xyz_orders` (
+  `date` TIMESTAMP,
+  `order_id` INT(100),
+  `payment_id` TINYINT(10),
+  `price` FLOAT,
+  `user_id` INT(10)
+);
+
+CREATE TABLE `xyz_payment` (
+  `pay_id` TINYINT(10) NOT NULL AUTO_INCREMENT,
+  `date` TIMESTAMP,
+  `pay_name` TEXT,
+  PRIMARY KEY  (`pay_id`)
+);
+
